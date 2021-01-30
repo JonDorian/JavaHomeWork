30fe13d217acbc8273f6e4b90f4968ff0ba3e1e3
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-01-30 14:27:07 +0300

    added additional package in lesson5, added Task1

---
+++ src/com/tms/lesson5/additional/Task1.java
@@ -0,0 +1,22 @@
+package com.tms.lesson5.additional;
+
+//Дополнительное задание №1.Начав тренировки, спортсмен в первый день пробежал 10 км.
+// Каждыйдень он увеличивал дневную норму на 10% нормы предыдущего дня.
+// Какойсуммарный путь пробежит спортсмен за 7 дней?
+
+public class Task1 {
+  public static void main(String[] args) {
+    float distance = 0;
+    float rounding = (float) Math.pow(10, 2);
+
+    for (int i = 1; i < 8; i++) {
+      if (i == 1) {
+        distance = 10;
+      } else {
+        distance = (float) Math.ceil((distance + ((distance * 10) / 100)) * rounding) / rounding;
+      }
+      System.out.print("День: " + i + "й, ");
+      System.out.println("расстояние: " + distance + "km");
+    }
+  }
+}
