b3d545539f731e245d0244d702d374240204e2ef
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-01-30 15:13:15 +0300

    corrected Task1

--- src/com/tms/lesson5/additional/Task1.java
+++ src/com/tms/lesson5/additional/Task1.java
@@ -8,6 +8,7 @@ public class Task1 {
   public static void main(String[] args) {
     float distance = 0;
     float rounding = (float) Math.pow(10, 2);
+    float totalDistance = 0;
 
     for (int i = 1; i < 8; i++) {
       if (i == 1) {
@@ -15,8 +16,11 @@ public class Task1 {
       } else {
         distance = (float) Math.ceil((distance + ((distance * 10) / 100)) * rounding) / rounding;
       }
+      totalDistance +=distance;
       System.out.print("День: " + i + "й, ");
       System.out.println("расстояние: " + distance + "km");
     }
+
+    System.out.println("Суммарный путь пробежки спортсмена за 7 дней: " + totalDistance + "km");
   }
 }
