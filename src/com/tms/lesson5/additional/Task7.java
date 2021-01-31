2df6c3a99e5f4ff6ce3e0be8a4ac1f68219bcffc
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-01-31 22:40:49 +0300

    corrected Task7

--- src/com/tms/lesson5/additional/Task7.java
+++ src/com/tms/lesson5/additional/Task7.java
@@ -6,10 +6,8 @@ public class Task7 {
   public static void main(String[] args) {
     int sum = 0;
 
-    for (int i = 1; i < 100; i++) {
-      if (i % 2 != 0) {
+    for (int i = 1; i < 100; i+=2) {
         sum +=i;
-      }
     }
 
     System.out.println("Сумма нечетных чисел от 1 до 99 равна: " + sum);
