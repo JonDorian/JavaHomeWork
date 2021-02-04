cf99e4b6c02cec02394221d9ea15c73313bcf08b
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-02-04 17:36:15 +0300

    corrected Task13 (LargerRandomNumber)

--- src/com/tms/lesson5/LargerRandomNumber.java
+++ src/com/tms/lesson5/LargerRandomNumber.java
@@ -4,27 +4,26 @@ package com.tms.lesson5;
 
 public class LargerRandomNumber {
   public static void main(String[] args) {
-    int[] array = new int[10];
+    byte[] array = new byte[10];
+    byte maxValue = 0;
+    byte maxValueIndex = 0;
 
     System.out.print("Исходеый массив чисел: ");
 
     for (int i = 0; i < array.length; i++) {
-      array[i] = (int) (Math.random() * 20);
+      array[i] = (byte) (Math.random() * 20);
       System.out.print(array[i] + " ");
     }
 
     System.out.println(" ");
 
-    for (int one = 0; one < 1; one++) {
-      for (int two = 0; two < array.length - 1 - one; two++) {
-        if (array[two] > array[two + 1]) {
-          int temp = array[two];
-          array[two] = array[two + 1];
-          array[two + 1] = temp;
-        }
+    for (byte one = 0; one < array.length; one++) {
+      if (maxValue < array[one]) {
+        maxValue = array[one];
+        maxValueIndex = one;
       }
     }
 
-    System.out.println("Наибольшее число в массиве: " + array[array.length - 1]);
+    System.out.println("Наибольшее число в массиве: " + maxValue + " под индексом: " + maxValueIndex);
   }
 }
