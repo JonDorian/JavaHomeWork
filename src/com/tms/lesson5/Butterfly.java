d1b4739f6a049406da64285418e1a54651716ebe
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-02-04 17:23:00 +0300

    corrected Task17 (Butterfly)

--- src/com/tms/lesson5/Butterfly.java
+++ src/com/tms/lesson5/Butterfly.java
@@ -4,24 +4,24 @@ package com.tms.lesson5;
 
 public class Butterfly {
   public static void main(String[] args) {
-    String[][] array = new String[9][9];
+    char[][] array = new char[9][9];
 
     for (int i = 0; i < array.length / 2 + 1; i++) {
       for (int j = i; j < array.length - i; j++) {
-        array[i][j] = "1";
+        array[i][j] = '1';
       }
     }
 
     for (int i = array.length - 1; i > array.length / 2 - 1; i--) {
       for (int j = i; j > array.length - i - 2; j--) {
-        array[i][j] = "1";
+        array[i][j] = '1';
       }
     }
 
     for (int i = 0; i < array.length; i++) {
       for (int j = 0; j < array[i].length; j++) {
-        if (array[i][j] == null) {
-          array[i][j] = "-";
+        if (array[i][j] == '\0') {
+          array[i][j] = ' ';
         }
         System.out.print(array[i][j] + " ");
       }
