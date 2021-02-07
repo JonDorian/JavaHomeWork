96cfdcfdb515f80410469e2b537e6993983b69f7
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-02-08 02:15:55 +0300

    corrected Task18 (PunctuationMarks)

--- src/com/tms/lesson6/PunctuationMarks.java
+++ src/com/tms/lesson6/PunctuationMarks.java
@@ -1,4 +1,6 @@
 package com.tms.lesson6;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //Задание №18 из методички №2. Найти в строке не только запятые, но и другие знаки препинания.
 //Подсчитать общее их количество.
@@ -9,29 +11,59 @@ public class PunctuationMarks {
     byte commas = 0;
     byte dash = 0;
     byte questions = 0;
+    byte space = 0;
+    byte semicolon = 0;
+    byte colon = 0;
+    byte parentheses = 0;
+    byte exclamationMark = 0;
     byte sumOfPunctuationMarks = 0;
-    char symbol;
-    String littleText = "Привет. Если в мире все бессмысленно, - сказала Алиса, - что мешает выдумать какой-нибудь смысл?";
+    String littleText = "Если в мире все бессмысленно?- сказала Алиса, - что мешает выдумать какой-нибудь смысл?";
 
-    for (int i = 0; i < littleText.length(); i++) {
-      symbol = littleText.charAt(i);
-      if (symbol == ',' || symbol == '-' || symbol == '.' || symbol == '?') {
-        if (symbol == ',') {
+    Pattern pat = Pattern.compile("(\\pP)|\\s");
+    Matcher match = pat.matcher(littleText);
+
+    while (match.find()) {
+      switch (match.group()) {
+        case ",":
           commas++;
-        } else if (symbol == '.') {
+          break;
+        case ".":
           dots++;
-        } else if (symbol == '-') {
+          break;
+        case "-":
           dash++;
-        } else if (symbol == '?') {
+          break;
+        case "?":
           questions++;
-        }
-        sumOfPunctuationMarks++;
+          break;
+        case " ":
+          space++;
+          break;
+        case ";":
+          semicolon++;
+          break;
+        case ":":
+          colon++;
+          break;
+        case ")":
+        case "(":
+          parentheses++;
+          break;
+        case "!":
+          exclamationMark++;
+          break;
       }
+      sumOfPunctuationMarks++;
     }
 
     System.out.println("Колличество запятых: " + commas);
+    System.out.println("Колличество точек с запятой: " + semicolon);
     System.out.println("Колличество точек: " + dots);
+    System.out.println("Колличество двоеточий: " + colon);
     System.out.println("Колличество тере: " + dash);
+    System.out.println("Колличество скобок: " + parentheses);
+    System.out.println("Колличество пробелов: " + space);
+    System.out.println("Колличество восклицательных знаков: " + exclamationMark);
     System.out.println("Колличество вопросительных знаков: " + questions);
     System.out.print("Общее колличество знаков припинания: " + sumOfPunctuationMarks);
   }
