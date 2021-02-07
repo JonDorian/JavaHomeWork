3c28ae40522b74dd35c2ac23a981b911fc63740f
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-02-07 23:59:07 +0300

    corrected Task12 (SumOfIntegers)

--- src/com/tms/lesson5/SumOfIntegers.java
+++ src/com/tms/lesson5/SumOfIntegers.java
@@ -1,4 +1,5 @@
 package com.tms.lesson5;
+import java.util.Scanner;
 
 //Задание № 12 из методички. Найдите сумму первых n целых чисел, которые делятся на 3.
 
@@ -6,18 +7,27 @@ public class SumOfIntegers {
   public static void main(String[] args) {
     int sum = 0;
     int counter = 0;
-    int randomNumber = (int) (Math.random() * 45 + 1);
+    boolean runFind = true;
 
-    for (int i = randomNumber; i < (randomNumber + 15); i++) {
-      if (i % 3 == 0) {
-        System.out.println("Число " + i + " делится на 3");
-        sum += i;
+    System.out.println("Укажите какое колличество чисел делящихся на 3 Вы хотите найти:");
+    Scanner choiceQuantity = new Scanner(System.in);
+    int quantityOfNumbers = choiceQuantity.nextInt();
+
+    System.out.println("Укажите c какого числа желайте начать поиск:");
+    Scanner number = new Scanner(System.in);
+    int numberStart = number.nextInt();
+
+    while (runFind) {
+      if (numberStart % 3 == 0) {
+        System.out.println("Число " + numberStart + " делится на 3");
+        sum += numberStart;
         counter++;
       }
-      if (counter == 5) {
-        System.out.println("Сумма 5ти первых целых чисел делящихся на 3 равна: " + sum);
+      if (counter == quantityOfNumbers) {
+        System.out.println("Сумма " + quantityOfNumbers + " первых целых чисел делящихся на 3 равна: " + sum);
         break;
       }
+      numberStart++;
     }
   }
 }
