6447db6acc53e20c586c7f2ec5244614e7d0f962
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-02-07 12:47:19 +0300

    corrected Task12 (SumOfIntegers)

--- src/com/tms/lesson5/SumOfIntegers.java
+++ src/com/tms/lesson5/SumOfIntegers.java
@@ -8,14 +8,14 @@ public class SumOfIntegers {
     int counter = 0;
     int randomNumber = (int) (Math.random() * 45 + 1);
 
-    for (int i = randomNumber; i < 50; i++) {
+    for (int i = randomNumber; i < (randomNumber + 15); i++) {
       if (i % 3 == 0) {
         System.out.println("Число " + i + " делится на 3");
         sum += i;
         counter++;
       }
-      if (counter == 2) {
-        System.out.println("Сумма первых целых чисел делящихся на 3 равна: " + sum);
+      if (counter == 5) {
+        System.out.println("Сумма 5ти первых целых чисел делящихся на 3 равна: " + sum);
         break;
       }
     }
