e5883f2d0b62e1cdc427296d34070cd4b5520856
Ilya Moiseyev <moiseyev.ilya@gmail.com>
2021-02-08 15:09:00 +0300

    corrected Task23 (ParagraphSearch)

--- src/com/tms/lesson6/ParagraphSearch.java
+++ src/com/tms/lesson6/ParagraphSearch.java
@@ -8,10 +8,10 @@ public class ParagraphSearch {
     String text = "<p id=”p1”>Lorem ipsum dolor sit amet, <p dsfsdf123>consectetur adipiscing elit</p>, sed do eiusmod tempor incididunt\n " +
             "ut labore et dolore magna aliqua</p>.<p id=”p1”>Ut enim ad minim veniam, quis nostrud exercitation ullamco\n " +
             "laboris nisi ut aliquip ex ea <p sadasdas>commodo</p> consequat.</p>Duis aute irure dolor in reprehenderit in voluptate\n " +
-            "velit esse cillum dolore eu <p id=”p1”>fugiat nulla pariatur.</p> <p id=”p1”>Excepteur sint occaecat cupidatat\n " +
+            "velit esse cillum dolore eu <p id=”p1” dfdsfsdcfc  dsfsfe32 >fugiat nulla pariatur.</p> <p id=”p1”>Excepteur sint occaecat cupidatat\n " +
             "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<p>";
 
-    text = text.replaceAll("<p(\\s(id)?\\s?=\\s?\\W\\S+\\W)?(.\\w+)?>", "<p>");
+    text = text.replaceAll("<((p(\\s(id)?\\s?=\\s?\\W\\S+\\W)?)|(p.+?))>", "<p>");
 
     System.out.print(text);
   }
